b98e8f05d7cb1450e13d3153a07e3c8514b092d3
Donat <hordonandron@gmail.com>
2019-08-30 10:56:05 +0200

    add fire laser bullet mechanism

--- spacebattle/GameScene.swift
+++ spacebattle/GameScene.swift
@@ -11,6 +11,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let player = SKSpriteNode(imageNamed: "playership")
+    
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode(imageNamed: "background")
@@ -19,11 +21,28 @@ class GameScene: SKScene {
         background.zPosition = 0
         self.addChild(background)
         
-        let player = SKSpriteNode(imageNamed: "playership")
         player.setScale(1.3)
         player.position = CGPoint(x: self.size.width / 2, y: self.size.height * 0.2)
         player.zPosition = 2
         self.addChild(player)
+    }
+    
+    func fire() {
+        
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(0.8)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        let move = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let remove = SKAction.removeFromParent()
+        let bulletSeq = SKAction.sequence([move, remove])
+        bullet.run(bulletSeq)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fire()
     }
     
 }
